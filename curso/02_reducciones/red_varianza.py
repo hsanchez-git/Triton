f39c317d516764f03e768312e00155624361c66d
hsanchez-git <h.sanchez.roda.rivera@gmail.com>
2026-07-14 10:39:53 +0200

    feat: implementar media y varianza por filas

--- curso/02_reducciones/red_varianza.py
+++ curso/02_reducciones/red_varianza.py
@@ -5,7 +5,7 @@ import triton.language as tl
 
 # Código que se ejecutará en la GPU
 @triton.jit
-def medio_matriz_kernel(matriz_ptr, salida_ptr, n_columnas, str_filas, str_columnas, str_salida_filas, BLOCK_SIZE: tl.constexpr):
+def varianza_matriz_kernel(matriz_ptr, salida_ptr, n_columnas, str_filas, str_columnas, str_salida_filas, BLOCK_SIZE: tl.constexpr):
     #comienzo que se repite
     fila = tl.program_id(axis=0)
     columnas = tl.arange(0, BLOCK_SIZE)
@@ -17,13 +17,17 @@ def medio_matriz_kernel(matriz_ptr, salida_ptr, n_columnas, str_filas, str_colum
     
     #el tl.sum de la mascara con me da un entero con la cantidad de true
     media=tl.sum(valores)/tl.sum(mascara)
+    #tl.where hace que se aplique la mascara a valores - media evitando que aplique a los 0.0'
+    diff = tl.where(mascara, valores - media, 0.0)
+    cuadrado=diff*diff
+    varianza=tl.sum(cuadrado)/tl.sum(mascara)
     offset_salida=str_salida_filas*fila
     #guardamos en el puntero de salida los elementos, como jno es un vector lo pasamos sin mascara
-    tl.store(salida_ptr + offset_salida, media)
+    tl.store(salida_ptr + offset_salida, varianza)
 
 
 # Código Python normal que prepara y lanza el kernel
-def medio_matriz(matriz):
+def varianza_matriz(matriz):
     #comprobaciones para que esten en cuda, la dimension y kla forma de la mtriz
     if not matriz.is_cuda:
         raise ValueError("La matriz debe estar en la GPU.")
@@ -52,11 +56,11 @@ def medio_matriz(matriz):
     str_filas = matriz.stride()[0]
     str_columnas = matriz.stride()[1]
     #hacemos la llamada y aunque de una especie de error es porque pylance no cuenta con triton
-    medio_matriz_kernel[grid](matriz, salida, n_columnas, str_filas, str_columnas, str_salida_filas, BLOCK_SIZE=BLOCK_SIZE)
+    varianza_matriz_kernel[grid](matriz, salida, n_columnas, str_filas, str_columnas, str_salida_filas, BLOCK_SIZE=BLOCK_SIZE)
     return salida
 
 
-def medio_matriz_pre():
+def varianza_matriz_pre():
     matriz = torch.tensor(
     [
         [-5.0, -2.0, -8.0, -3.0, -6.0],
@@ -71,17 +75,22 @@ def medio_matriz_pre():
     print ("Su stride es: ", matriz.stride())
     print ("¿Es contigua? ", matriz.is_contiguous())
 
-    resultado = medio_matriz(matriz)
-    esperado = matriz.mean(dim=1)
-
-    torch.testing.assert_close(
-        resultado,
-        esperado,
+    resultado = varianza_matriz(matriz)
+    esperado = torch.var(
+    matriz,
+    dim=1,
+    correction=0,
     )
+
+    print("Resultado Triton:", resultado)
+    print("Resultado PyTorch:", esperado)
+
+    torch.testing.assert_close(resultado, esperado)
+    print("Prueba de varianza superada")
     print("prueba de la matriz: \n", matriz, "\nel resultado es: \n", resultado)
 
 def main():
-    medio_matriz_pre()
+    varianza_matriz_pre()
 
 
 if __name__ == "__main__":
